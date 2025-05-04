ce12a6c685391fe6aad23e5d1f63c591a07f1fe5
David Ferlay <dferlay@skilld.cloud>
2025-05-04 22:43:36 +0200

    Adding vercel and fixing inputs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello!");
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run(): Promise<void> {
+  console.log("XXXXXXXX");
+  const token: string = core.getInput("github-token");
+  const label: string = core.getInput("label");
+
+  const octokit = github.getOctokit(token);
+  const pullRequest = github.context.payload.pull_request;
+
+  try {
+    if (!pullRequest) {
+      core.setFailed("This action can only run on Pull Requests.");
+      return; // prevent further execution
+    }
+
+    await octokit.rest.issues.addLabels({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: pullRequest.number,
+      labels: [label],
+    });
+  } catch (error) {
+    core.setFailed((error as Error)?.message ?? "Unknown error");
+  }
+}
+
+run();
